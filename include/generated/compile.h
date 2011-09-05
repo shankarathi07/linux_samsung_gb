6ada8ab21fbb5aabd5db2b892ca4a1bc3641b835
Shankar <mathkid95@gmail.com>
2011-09-05 00:21:28 -0500

    Matr1x kernel sources

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Sun Sep 4 23:59:59 CDT 2011"
+#define LINUX_COMPILE_TIME "23:59:59"
+#define LINUX_COMPILE_BY "shankar"
+#define LINUX_COMPILE_HOST "Matr1x.local"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
