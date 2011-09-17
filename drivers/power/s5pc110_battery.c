4eb969e458afa3e023d5cd907a8de57fa0ad9821
Shankar <mathkid95@gmail.com>
2011-09-16 23:20:54 -0500

    Update all of Ezekeel's current mods, and add his BLX mod

--- drivers/power/s5pc110_battery.c
+++ drivers/power/s5pc110_battery.c
@@ -47,6 +47,10 @@
 #include "s5pc110_battery.h"
 #include <linux/mfd/max8998.h>
 
+#ifdef CONFIG_BLX
+#include <linux/blx.h>
+#endif
+
 #define POLLING_INTERVAL	1000
 #define ADC_TOTAL_COUNT		10
 #define ADC_DATA_ARR_SIZE	6
@@ -389,26 +393,35 @@ static void s3c_bat_discharge_reason(struct chg_data *chg)
 
 	discharge_reason = chg->bat_info.dis_reason & 0xf;
 
-	if (discharge_reason & DISCONNECT_BAT_FULL &&
+	if (discharge_reason == DISCONNECT_BAT_FULL &&
 			chg->bat_info.batt_vcell < RECHARGE_COND_VOLTAGE)
 		chg->bat_info.dis_reason &= ~DISCONNECT_BAT_FULL;
 
-	if (discharge_reason & DISCONNECT_TEMP_OVERHEAT &&
+	if (discharge_reason == DISCONNECT_TEMP_OVERHEAT &&
 			chg->bat_info.batt_temp <=
 			HIGH_RECOVER_TEMP)
 		chg->bat_info.dis_reason &= ~DISCONNECT_TEMP_OVERHEAT;
 
-	if (discharge_reason & DISCONNECT_TEMP_FREEZE &&
+	if (discharge_reason == DISCONNECT_TEMP_FREEZE &&
 			chg->bat_info.batt_temp >=
 			LOW_RECOVER_TEMP)
 		chg->bat_info.dis_reason &= ~DISCONNECT_TEMP_FREEZE;
 
-	if (discharge_reason & DISCONNECT_OVER_TIME &&
+	if (discharge_reason == DISCONNECT_OVER_TIME &&
 			chg->bat_info.batt_vcell < RECHARGE_COND_VOLTAGE)
 		chg->bat_info.dis_reason &= ~DISCONNECT_OVER_TIME;
 
 	if (chg->set_batt_full)
 		chg->bat_info.dis_reason |= DISCONNECT_BAT_FULL;
+
+#ifdef CONFIG_BLX
+	if (chg->bat_info.batt_soc >= get_charginglimit())
+	    {
+		chg->bat_info.dis_reason |= DISCONNECT_BAT_FULL;
+
+		chg->bat_info.batt_is_full = true;
+	    }
+#endif
 
 	if (chg->bat_info.batt_health != POWER_SUPPLY_HEALTH_GOOD)
 		chg->bat_info.dis_reason |= chg->bat_info.batt_health ==
