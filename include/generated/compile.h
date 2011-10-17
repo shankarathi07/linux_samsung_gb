018ea3cb1843834fb5bb80d272d804a701c8f14c
Shankar <mathkid95@gmail.com>
2011-10-16 22:00:02 -0500

    Add Deep Idle by Ezekeel

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 4 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 PREEMPT Sat Sep 17 01:17:44 CDT 2011"
-#define LINUX_COMPILE_TIME "01:17:44"
+#define UTS_VERSION "#4 PREEMPT Wed Oct 12 18:41:01 CDT 2011"
+#define LINUX_COMPILE_TIME "18:41:01"
 #define LINUX_COMPILE_BY "shankar"
 #define LINUX_COMPILE_HOST "Matr1x.local"
 #define LINUX_COMPILE_DOMAIN
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.14-ck1-bfs-gac6796d-dirty"
+#define UTS_RELEASE "2.6.35.14-ck1-bfs-g3a634d0-dirty"
