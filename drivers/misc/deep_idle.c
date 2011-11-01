4fcb41166c9a54c59386a506681e991061ca024f
Shankar <mathkid95@gmail.com>
2011-11-01 17:21:08 -0500

    Update all of Ezekeel's mods: part 3 	modified:   arch/arm/mach-s5pv210/cpuidle.c 	modified:   drivers/misc/deep_idle.c 	modified:   include/linux/deep_idle.h

--- drivers/misc/deep_idle.c
+++ drivers/misc/deep_idle.c
@@ -10,11 +10,18 @@
 #include <linux/init.h>
 #include <linux/device.h>
 #include <linux/miscdevice.h>
+#include <linux/mutex.h>
 #include <linux/deep_idle.h>
 
-#define DEEPIDLE_VERSION 1
+#define DEEPIDLE_VERSION 2
 
-static bool deepidle_enabled = true;
+#define NUM_IDLESTATES 3
+
+static DEFINE_MUTEX(lock);
+
+static bool deepidle_enabled = false;
+
+static unsigned long long num_idlecalls[NUM_IDLESTATES], time_in_idlestate[NUM_IDLESTATES]; 
 
 static ssize_t deepidle_status_read(struct device * dev, struct device_attribute * attr, char * buf)
 {
@@ -52,17 +59,83 @@ static ssize_t deepidle_status_write(struct device * dev, struct device_attribut
     return size;
 }
 
+static ssize_t show_idle_stats(struct device * dev, struct device_attribute * attr, char * buf)
+{
+    int i;
+    unsigned long long msecs_in_idlestate[NUM_IDLESTATES], avg_in_idlestate[NUM_IDLESTATES];
+
+    mutex_lock(&lock);
+
+    for (i = 0; i < NUM_IDLESTATES; i++) {
+	msecs_in_idlestate[i] = time_in_idlestate[i] + 500;
+	do_div(msecs_in_idlestate[i], 1000);
+	if (num_idlecalls[i] == 0) {
+	    avg_in_idlestate[i] = 0;
+	} else {
+	    avg_in_idlestate[i] = msecs_in_idlestate[i];
+	    do_div(avg_in_idlestate[i], num_idlecalls[i]);
+	}
+    }
+
+    mutex_unlock(&lock);
+
+    return sprintf(buf, "idle state             total (average)\n===================================================\nIDLE                   %llums (%llums)\nDEEP IDLE (TOP=ON)     %llums (%llums)\nDEEP IDLE (TOP=OFF)    %llums (%llums)\n",
+		   msecs_in_idlestate[0], avg_in_idlestate[0], msecs_in_idlestate[1], avg_in_idlestate[1], msecs_in_idlestate[2], avg_in_idlestate[2]);
+}
+
+static void reset_stats(void)
+{
+    int i;
+
+    for (i = 0; i < NUM_IDLESTATES; i++)
+	{
+	    num_idlecalls[i] = 0;
+	    time_in_idlestate[i] = 0;
+	}
+
+    return;
+}   
+
+static ssize_t reset_idle_stats(struct device * dev, struct device_attribute * attr, const char * buf, size_t size)
+{
+    unsigned int data;
+
+    if(sscanf(buf, "%u\n", &data) == 1) 
+	{
+	    if (data == 1)
+		{
+		    mutex_lock(&lock);
+		    reset_stats();
+		    mutex_unlock(&lock);
+		}
+	    else 
+		{
+		    pr_info("%s: invalid input range %u\n", __FUNCTION__, data);
+		}
+	} 
+    else 
+	{
+	    pr_info("%s: invalid input\n", __FUNCTION__);
+	}
+
+    return size;
+}
+
 static ssize_t deepidle_version(struct device * dev, struct device_attribute * attr, char * buf)
 {
     return sprintf(buf, "%u\n", DEEPIDLE_VERSION);
 }
 
 static DEVICE_ATTR(enabled, S_IRUGO | S_IWUGO, deepidle_status_read, deepidle_status_write);
+static DEVICE_ATTR(idle_stats, S_IRUGO , show_idle_stats, NULL);
+static DEVICE_ATTR(reset_stats, S_IWUGO , NULL, reset_idle_stats);
 static DEVICE_ATTR(version, S_IRUGO , deepidle_version, NULL);
 
 static struct attribute *deepidle_attributes[] = 
     {
 	&dev_attr_enabled.attr,
+	&dev_attr_idle_stats.attr,
+	&dev_attr_reset_stats.attr,
 	&dev_attr_version.attr,
 	NULL
     };
@@ -84,6 +157,24 @@ bool deepidle_is_enabled(void)
 }
 EXPORT_SYMBOL(deepidle_is_enabled);
 
+void report_idle_time(int idle_state, int idle_time)
+{
+    mutex_lock(&lock);
+
+    num_idlecalls[idle_state]++;
+    time_in_idlestate[idle_state] += (unsigned long long)idle_time;
+
+    if (num_idlecalls[idle_state] == 0 || time_in_idlestate[idle_state] < (unsigned long long)idle_time)
+	{
+	    reset_stats();
+	}
+
+    mutex_unlock(&lock);
+
+    return;
+}
+EXPORT_SYMBOL(report_idle_time);
+
 static int __init deepidle_init(void)
 {
     int ret;
@@ -104,6 +195,10 @@ static int __init deepidle_init(void)
 	    pr_err("%s sysfs_create_group fail\n", __FUNCTION__);
 	    pr_err("Failed to create sysfs group for device (%s)!\n", deepidle_device.name);
 	}
+
+    mutex_lock(&lock);
+    reset_stats();
+    mutex_unlock(&lock);
 
     return 0;
 }
