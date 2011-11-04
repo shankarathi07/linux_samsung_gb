50cf912e31a6f003d9e02359bbf9e246bdd9e5fc
Shankar <mathkid95@gmail.com>
2011-11-03 20:09:40 -0500

    Nope, these new voltages are bad. Let's go back to the old ones 	modified:   arch/arm/mach-s5pv210/cpu-freq.c 	modified:   drivers/cpufreq/cpufreq.c

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -33,7 +33,7 @@
 						"cpufreq-core", msg)
 
 /* UV */
-int exp_UV_mV[8] = { 1460000,1390000,1325000, 1225000, 1190000, 1040000, 915000, 915000};
+int exp_UV_mV[8] = { 1450000,1380000,1315000, 1215000, 1180000, 1030000, 905000, 905000};
 
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
